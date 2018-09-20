c028d0cda86d21e4632ebd5b4429d68f6b4d0805
nowatewat <nowatewat@gmail.com>
2018-09-20 16:14:45 -0600

    limited handle to 15 chars and added chatbox

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
+//so now i need to be getting elements by id or whatever instead of creating
+//can and if so how does css address elements created by js script?
 var socket = new WebSocket('ws://127.0.0.1:5000');
 var chatinput = document.createElement('input');
 chatinput.type = 'text';
 document.body.appendChild(chatinput);
 var username;
-
+var chatoutput = document.getElementById('chatoutput');
 socket.addEventListener('open', function(){
   console.log('I\'m connected');
   var button = document.createElement('button');
@@ -12,8 +14,7 @@ socket.addEventListener('open', function(){
   //should event listener be in here?
   button.addEventListener('click', function(){
     //CONDITIONAL BELOW COPYPASTED FROM GITHUB
-    //Havent actually limited to 15 chars
-    if(username === undefined && !(chatinput.value.match(/^[0-9a-z]+$/))){
+    if(username === undefined || (!(chatinput.value.match(/^[0-9a-z]+$/))) || chatinput.value.toString().length > 15 ){
       alert('Invalid user name. Please only use alpha numeric characters. Usernames may be up to 15 characters long.');
     }else if(username === undefined) {
       socket.send(chatinput.value.toString().trim()+'\n');
@@ -21,6 +22,8 @@ socket.addEventListener('open', function(){
       username = chatinput.value.toString().trim();
     }else{
       socket.send(chatinput.value.toString().trim()+'\n');
+      chatoutput.append(chatinput.value.toString(),trim()+'\n');
+      
     }
     chatinput.value = '';
   });
